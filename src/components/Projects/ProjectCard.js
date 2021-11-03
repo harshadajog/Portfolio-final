bc7a88fa4f09def02e3ca7c1ad1bfa383d8ed3e4
Netapp DPG <netappdpg@Netapps-Air.lan>
2021-11-03 09:59:26 -0700

    removed the unused variable

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -3,7 +3,7 @@ import './ProjectCard.style.scss';
 import '../../App.scss';
 
 function ProjectCard(props) {
-  const { name, image, description, details, tools, git, link } = props.data;
+  const { name, image, details, tools, git, link } = props.data;
   const toolList = tools.map((tool, i) => <div key={i}>{tool}</div>);
   const detailList = details.map((detail, i) => <li key={i}>{detail}</li>);
 
